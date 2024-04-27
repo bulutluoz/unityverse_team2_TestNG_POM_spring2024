4384532a85af9a9cbd53be7ab6d4d3f3af1f68e9
bulutluoz <abbulutluoz@gmail.com>
2024-04-27 18:40:23 +0200

    day10 sonu

--- src/test/java/tests/day10_TestNGFramework_Assertions/C03_DinamikPozitifLoginTesti.java
+++ src/test/java/tests/day10_TestNGFramework_Assertions/C03_DinamikPozitifLoginTesti.java
@@ -1,17 +1,32 @@
 package tests.day10_TestNGFramework_Assertions;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import pages.TestotomasyonuPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+import utilities.ReusableMethods;
 
 public class C03_DinamikPozitifLoginTesti {
 
     @Test
     public void pozitifLoginTesti(){
         //1- https://www.testotomasyonu.com/ anasayfasina gidin
+        Driver.getDriver().get(ConfigReader.getProperty("toUrl"));
         //2- account linkine basin
+        TestotomasyonuPage testotomasyonuPage = new TestotomasyonuPage();
+        testotomasyonuPage.accountLinki.click();
         //3- Kullanici email'i olarak gecerli email girin
+        testotomasyonuPage.emailKutusu.sendKeys(ConfigReader.getProperty("toGecerliEmail"));
         //4- Kullanici sifresi olarak gecerli password girin
+        testotomasyonuPage.passwordKutusu.sendKeys(ConfigReader.getProperty("toGecerliPassword"));
         //5- Login butonuna basarak login olun
+        testotomasyonuPage.loginSayfasiLoginButonu.click();
         //6- Basarili olarak giris yapilabildigini test edin
-        //7- Sayfayi kapatin
+        Assert.assertTrue(testotomasyonuPage.logoutButonu.isEnabled());
+        //7- logout olup, sayfayi kapatin
+        testotomasyonuPage.logoutButonu.click();
+        Driver.quitDriver();
+
     }
 }
---
+++ src/test/java/tests/day10_TestNGFramework_Assertions/C04_DinamikNegatifLoginTesti.java
@@ -0,0 +1,28 @@
+package tests.day10_TestNGFramework_Assertions;
+
+import org.testng.annotations.Test;
+
+public class C04_DinamikNegatifLoginTesti {
+
+    //1- https://www.testotomasyonu.com/ anasayfasina gidin
+    //2- account linkine basin
+    //3-  3 farkli test method’u olusturun.
+    //	- gecerli username, gecersiz password
+    //	- gecersiz username, gecerli password
+    //	- gecersiz username, gecersiz password.
+    //4- Login butonuna basarak login olun
+    //5- Basarili olarak giris yapilamadigini test edin
+
+
+    @Test
+    public void gecersizPasswordTesti(){
+        //1- https://www.testotomasyonu.com/ anasayfasina gidin
+        //2- account linkine basin
+        //3- gecerli username, gecersiz password yazin
+        //4- Login butonuna basarak login olun
+        //5- Basarili olarak giris yapilamadigini test edin
+        //6- Sayfayi kapatin
+
+
+    }
+}
