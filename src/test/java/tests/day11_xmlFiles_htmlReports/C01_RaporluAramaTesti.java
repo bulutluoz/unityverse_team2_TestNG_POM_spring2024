aaed5be9118217f8cbbd7506ed09eff9a64e7d13
bulutluoz <abbulutluoz@gmail.com>
2024-04-28 19:57:20 +0200

    day11 sonu

---
+++ src/test/java/tests/day11_xmlFiles_htmlReports/C01_RaporluAramaTesti.java
@@ -0,0 +1,20 @@
+package tests.day11_xmlFiles_htmlReports;
+
+import org.testng.annotations.Test;
+
+public class C01_RaporluAramaTesti {
+
+    @Test
+    public void raporluAramaTesti(){
+
+        // 1. testotomasyonu anasayfaya gidin
+        // 2. Title'in Test icerdigini dogrulayin
+        // 3. url'in https://www.testotomasyonu.com oldugunu dogrulayin
+        // 4. arama kutusunun kullanilabilir durumda oldugunu dogrulayin
+        // 5. belirlenmis aranacak kelimeyi aratip urun bulundugunu dogrulayin
+        // 6. Nutella aratip, urun bulunamadigini dogrulayin
+        // 7. Sayfayi kapatin
+    }
+
+
+}
