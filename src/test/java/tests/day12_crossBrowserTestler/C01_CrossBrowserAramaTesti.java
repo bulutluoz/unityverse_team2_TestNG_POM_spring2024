438c4cbcabcea264edbe95faa1398e8c65683845
bulutluoz <abbulutluoz@gmail.com>
2024-05-04 18:48:42 +0200

    day12 sonu

---
+++ src/test/java/tests/day12_crossBrowserTestler/C01_CrossBrowserAramaTesti.java
@@ -0,0 +1,30 @@
+package tests.day12_crossBrowserTestler;
+
+import org.testng.annotations.Test;
+
+public class C01_CrossBrowserAramaTesti {
+
+    @Test
+    public void aramaTesti(){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
